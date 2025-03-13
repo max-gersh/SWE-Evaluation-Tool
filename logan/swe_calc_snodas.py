365edf93745a280a8e77474453f8b5fefc429596
Logan Stephenson <blatstephenson@gmail.com>
2025-03-13 18:20:39 +0100

    Added loops to calculate SWE volume for HUC8 polygons

--- logan/swe_calc_snodas.py
+++ logan/swe_calc_snodas.py
@@ -43,6 +43,10 @@ HUC8_polys = ['Rio Grande Headwaters', 'Alamosa-Trinchera',
               'Elephant Butte Reservoir', 'Rio Puerco']
 HUC8 = HUC8[HUC8['Name'].isin(HUC8_polys)]
 
+## sort by polygon order
+HUC8 = HUC8.set_index('Name', drop = False)
+HUC8 = HUC8.reindex(HUC8_polys)
+
 ## confirm CRS are all equal
 OSE.crs == HUC8.crs == HUC6.crs == data.rio.crs
 
@@ -62,22 +66,36 @@ plt.show()
 
 ###################################################
 
-## SWE calc no reprojection
-poly_clip = clipped_data.rio.clip([HUC8.iloc[1].geometry], HUC8.crs)
-
-## plot clipped polygon
-f, ax = plt.subplots()
-poly_clip.plot(cmap="Blues",
-                 ax=ax)
-plt.show()
-
-## calc SWE
-mean_value = np.nanmean(poly_clip) / 1000
-swe_volume = mean_value * (HUC8.iloc[1].AreaSqKm*1000000)
-swe_volume
+## latitude longitude raster
+## for loop to calculate swe for each HUC8 basin
+cols = ['HUC8 Subbasin', 'Volume (AF)']
+data_lat_lon = []
+for i in range(len(HUC8.index)):
+    ## SWE calc no reprojection
+    poly_clip = clipped_data.rio.clip([HUC8.iloc[i].geometry], HUC8.crs)
+    
+    ## plot clipped polygon
+    f, ax = plt.subplots()
+    poly_clip.plot(cmap="Blues",
+                     ax=ax)
+    plt.show()
+    
+    ## calc SWE
+    mean_value = np.nanmean(poly_clip) / 1000
+    swe_volume = mean_value * (HUC8.iloc[i].AreaSqKm*1000000)
+    swe_volume
+    
+    swe_volume_af = round(swe_volume * 0.000810714)
+    swe_volume_af
+    
+    data_lat_lon.append([HUC8.iloc[i].Name, swe_volume_af])
+    
+swe_df = pd.DataFrame(data_lat_lon, columns=cols)
+swe_df
 
 ####################################################3
 
+## AEA rasters (EPSG: 5070)
 ## SWE calc with reprojection
 aea_crs = "EPSG:5070"
 data_reprojected = clipped_data.rio.reproject(
@@ -97,23 +115,38 @@ HUC8_aea.plot(color='None',
                     ax=ax)
 plt.show()
 
-## clip AEA raster to polygon 1
-poly_clip_aea = data_reprojected.rio.clip([HUC8_aea.iloc[1].geometry], HUC8_aea.crs)
+## for loop to calculate swe for each HUC8 basin
+cols_aea = ['HUC8 Subbasin', 'Reproj Volume (AF)']
+data_aea = []
+for i in range(len(HUC8_aea.index)):
+    ## clip AEA raster to polygon 1
+    poly_clip_aea = data_reprojected.rio.clip([HUC8_aea.iloc[i].geometry], HUC8_aea.crs)
+    
+    ## plot clipped polygon
+    f, ax = plt.subplots()
+    poly_clip_aea.plot(cmap="Blues",
+                     ax=ax)
+    plt.show()
+    
+    
+    pixel_size_x, pixel_size_y = poly_clip_aea.rio.resolution()
+    pixel_area = abs(pixel_size_x * pixel_size_y)
+    
+    swe_volume_reproj = (np.nansum(poly_clip_aea.values) / 1000) * pixel_area
+    swe_volume_reproj
+    
+    swe_volume_reproj_af = round(swe_volume_reproj * 0.000810714)
+    swe_volume_reproj_af 
+    
+    data_aea.append([HUC8_aea.iloc[i].Name, swe_volume_reproj_af])
+    
+swe_df_aea = pd.DataFrame(data_aea, columns=cols_aea)
+swe_df_aea
 
-## plot clipped polygon
-f, ax = plt.subplots()
-poly_clip_aea.plot(cmap="Blues",
-                 ax=ax)
-plt.show()
 
-
-pixel_size_x, pixel_size_y = poly_clip_aea.rio.resolution()
-pixel_area = abs(pixel_size_x * pixel_size_y)
-
-total_swe = (np.nansum(poly_clip_aea.values) / 1000) * pixel_area
-total_swe
-
-
+## 
+swe_data_full = pd.concat([swe_df, swe_df_aea['Reproj Volume (AF)']],
+                          axis = 1)
 
 
 
